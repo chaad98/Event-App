bbcd9036f2ac339c6367ba0bba7e5a550c268f3f
chad <faritzchad@yahoo.com>
2024-05-10 12:17:12 +0800

    Chapter 3 - Added Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Evently",
+  title: "Evently OwLabs",
   description: "Evently is a platform for event management",
   icons: {
     icon: "/assets/images/logo.svg",
